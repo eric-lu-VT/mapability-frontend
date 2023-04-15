c3fb84880182a3baf1c837387dcf493b75dc21d2
Eric Lu <68611574+eric-lu-VT@users.noreply.github.com>
2023-04-15 15:18:50 -0400

    Revert "changed screen index to tsx"

---
+++ src/screens/index.jsx
@@ -0,0 +1,30 @@
+import {
+  SignInPage,
+  SignUpPage,
+  VerifyPage,
+} from './AuthScreens';
+
+import { 
+  FrontPage,
+  ForbiddenPage,
+} from './BaseScreens';
+
+import {
+  MapPage,
+} from './MapStackScreens';
+
+import {
+  UsersPage,
+  ResourcesPage,
+} from './ResourceStackScreens';
+
+export { 
+  SignInPage,
+  SignUpPage,
+  VerifyPage,
+  FrontPage,
+  ForbiddenPage,
+  MapPage,
+  UsersPage,
+  ResourcesPage,
+};
